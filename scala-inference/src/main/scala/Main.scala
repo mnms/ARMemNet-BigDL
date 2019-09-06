1f989f87652fb786cd1973fe9611bf2b7c5c2be8
yangw <yang3.wang@intel.com>
2019-09-06 10:38:26 +0800

    add test for spark

--- scala-inference/src/main/scala/Main.scala
+++ scala-inference/src/main/scala/Main.scala
@@ -1,24 +1,16 @@
 import java.nio.file.Paths
 
+import com.intel.analytics.bigdl.models.utils.ModelBroadcast
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.utils.T
+import com.intel.analytics.zoo.common.NNContext
 import com.intel.analytics.zoo.pipeline.api.net.TFNet
 import org.jetbrains.bio.npy.NpyFile
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
 
-    val tfnetPath = args(0)
-    val testXPath = args(1)
-    val testMPath = args(2)
-    val batchSize = args(3).toInt
-
-    println(s"tfnet path is ${tfnetPath}")
-    println(s"test_x path is ${testXPath}")
-    println(s"test_m path is ${testMPath}")
-    println(s"batch size is ${batchSize}")
-
+  def testForLocal(tfnetPath: String, testXPath: String, testMPath: String, batchSize: Int) = {
 
     val xPath: java.nio.file.Path = Paths.get(testXPath)
 
@@ -49,5 +41,78 @@ object Main {
 
     val end = System.nanoTime()
     println(s"time is ${(end - start)/1.0e9}s")
+
+  }
+
+  def testForSpark(tfnetPath: String, testXPath: String, testMPath: String, batchSize: Int) = {
+
+    val sc = NNContext.initNNContext()
+
+    val xPath: java.nio.file.Path = Paths.get(testXPath)
+
+    val testX = NpyFile.read(xPath, Int.MaxValue)
+
+    val mPath: java.nio.file.Path = Paths.get(testMPath)
+
+    val testM = NpyFile.read(mPath, Int.MaxValue)
+
+    val length = testM.getShape()(0)
+
+    val tfnet = TFNet(tfnetPath)
+
+    val btfnet = ModelBroadcast[Float]().broadcast(sc, tfnet)
+
+    val batchedX = testX.asFloatArray().grouped(batchSize * 10 * 8).toSeq
+
+    val batchedM = testM.asFloatArray().grouped(batchSize * 77 * 8).toSeq
+
+    val localInput = batchedX.zip(batchedM).map { case (x, m) =>
+      val inputXData = Tensor[Float](x, Array(x.length/10/8, 10, 8))
+      val inputMData = Tensor[Float](m, Array(m.length/77/8, 77, 8))
+      val input = T(inputXData, inputMData)
+      input
+    }
+
+    val inputRdd = sc.parallelize(localInput, 4).cache()
+    inputRdd.count()
+
+
+    val modelRdd = inputRdd.mapPartitions { part =>
+      val localTFNet = btfnet.value()
+      Iterator.single(localTFNet)
+    }.cache()
+
+    modelRdd.count()
+
+    val results = inputRdd.zipPartitions(modelRdd) { case (dataIter, modelIter) =>
+      val model = modelIter.next()
+      val result = dataIter.toArray.map(model.forward)
+      result.toIterator
+    }
+
+    var start = System.nanoTime()
+    results.collect()
+    var end = System.nanoTime()
+    println(s"time is ${(end - start)/1.0e9}s")
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val tfnetPath = args(0)
+    val testXPath = args(1)
+    val testMPath = args(2)
+    val batchSize = args(3).toInt
+    val islocal = args(4).toBoolean
+
+    println(s"tfnet path is ${tfnetPath}")
+    println(s"test_x path is ${testXPath}")
+    println(s"test_m path is ${testMPath}")
+    println(s"batch size is ${batchSize}")
+
+    if (islocal) {
+      testForLocal(tfnetPath, testXPath, testMPath, batchSize)
+    } else {
+      testForSpark(tfnetPath, testXPath, testMPath, batchSize)
+    }
   }
 }
